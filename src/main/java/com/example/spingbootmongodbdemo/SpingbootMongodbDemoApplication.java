948c759d51cb4abfebd105587d281e95f5b76c0d
TestVagrant <testvagrant@TestVagrants-MacBook-Pro.local>
2022-10-03 20:43:44 +0530

    Initial Commit

---
+++ src/main/java/com/example/spingbootmongodbdemo/SpingbootMongodbDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spingbootmongodbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingbootMongodbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingbootMongodbDemoApplication.class, args);
+	}
+
+}
